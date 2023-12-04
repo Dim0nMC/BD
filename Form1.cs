4a9395eea8aec88ef7f6c48b44ee48302f26d1a4
Dim0nMC <seliverstovd2003@gmail.com>
2023-12-05 01:32:12 +0300

    s

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Npgsql;
 
 namespace BD
 {
@@ -15,11 +16,25 @@ namespace BD
         public Form1()
         {
             InitializeComponent();
+            NpgsqlConnection conn = new NpgsqlConnection("Server=localhost;Port=5432;Database=postgres;User Id=postgres;password=dimonos2003");
+            conn.Open();
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.Connection = conn; ;
+            comm.CommandType=CommandType.Text;
+            comm.CommandText = "select * from Поставщики";
+            NpgsqlDataReader reader = comm.ExecuteReader(); 
+            if(reader != null )
+            {
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+                dataGridView1.DataSource = dt;
+            }
+            comm.Dispose();
+            conn.Close();
         }
-
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-
         }
     }
 }
