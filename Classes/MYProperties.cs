c87abfb34fc95d1de6971346160923d13b1aa67d
Dim0nMC <seliverstovd2003@gmail.com>
2023-12-05 17:07:23 +0300

    s

---
+++ Classes/MYProperties.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BD
+{
+    static class MYProperties
+    {
+        public static string password = "dimonos2003";
+    }
+}
